40a8c1c3cce841dff1f03bdb5de5703db6486495
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-18 17:22:35 +0530

    protected routes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+
+import { 
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo
+ } from "@angular/fire/auth-guard";
 
 
-const routes: Routes = [];
+
+
+
+
+
+const redirectUnauthorizedToSignin=()=>redirectUnauthorizedTo(['signin'])
+const redirectLoggedInToHome=()=>redirectLoggedInTo([''])
+
+
+const routes: Routes = [
+
+                {
+                  path:'signin',
+                  component:SigninComponent,
+                  canActivate:[AngularFireAuthGuard],
+                  data:{authGuardPipe:redirectLoggedInToHome},
+                },
+                {
+                  path:'signup',
+                  component:SignupComponent,
+                },
+                {
+                  path:'',
+                  component:HomeComponent,
+                  canActivate:[AngularFireAuthGuard],
+                  data:{authGuardPipe:redirectUnauthorizedToSignin},
+                },
+                {
+                  path:'**',
+                  component:NotfoundComponent,
+                },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth:AuthService,
+    private router :Router,
+    private toastr : ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f:NgForm){
+    const {email,password}= f.form.value
+    this.auth.signup(email,password)
+    .then((res)=>{
+      this.router.navigateByUrl('/')
+      this.toastr.success('Successfully Signedup')
+    })
+    .catch((err)=>{
+      console.log(err.message);
+      this.toastr.error('Signup Failed')
+      
+    })
+
+
+
   }
 
 }
