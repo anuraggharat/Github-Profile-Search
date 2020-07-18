4405b0e8bf43d2a6bafa474e5be5815e3968159c
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-18 11:52:48 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { UsercardComponent } from './components/usercard/usercard.component';
+
+//form and httpclient
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//firebase related imports
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+
+//for toast
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReposComponent,
+    UsercardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD6NDe7O_QAvjoQfiz_Xmcb9tigsk7g7ag",
+    authDomain: "lcoproject1.firebaseapp.com",
+    databaseURL: "https://lcoproject1.firebaseio.com",
+    projectId: "lcoproject1",
+    storageBucket: "lcoproject1.appspot.com",
+    messagingSenderId: "716234579578",
+    appId: "1:716234579578:web:f8dc07b20cf547811ee5c3"
+  }
 };
 
 /*
